5f9d03659d2bb3e6aae5ac0574fb09bb5287e62b
GithubUserXyz <github_user_xyz@aol.com>
2023-06-24 21:54:43 +0900

    change rakute_api.dart

--- lib/model/rakuten_api.dart
+++ lib/model/rakuten_api.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
 import 'package:http/http.dart';
 
@@ -5,18 +7,67 @@ import 'package:rakuten_books_viewer/application_id.dart';
 
 import '../main.dart';
 
+class RakutenBooksItem {
+  String title;
+  String itemUrl;
+  String largeImageUrl;
+
+  RakutenBooksItem({
+    required this.title,
+    required this.itemUrl,
+    required this.largeImageUrl,
+  });
+
+  factory RakutenBooksItem.fromJson(Map<String, dynamic> json) {
+    return RakutenBooksItem(
+        title: json["title"],
+        itemUrl: json["itemUrl"],
+        largeImageUrl: json["largeImageUrl"]);
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data["title"] = title;
+    data["itemUrl"] = itemUrl;
+    data["largeImageUrl"] = largeImageUrl;
+    return data;
+  }
+}
+
+class RakutenApiResponse {
+  List<dynamic> genreInformation;
+
+  RakutenApiResponse({required this.genreInformation});
+
+  factory RakutenApiResponse.fromJson(Map<String, dynamic> json) {
+    return RakutenApiResponse(genreInformation: json["GenreInformation"]);
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data["GenreInformation"] = genreInformation;
+    return data;
+  }
+}
+
 class RakutenApi {
-  Future<Response> searchBooksByTitle(String title) async {
+  Future<Response> searchBooksByTitle(
+      String title, List<RakutenBooksItem> listItems) async {
     var url = Uri.https(
         "app.rakuten.co.jp",
         "/services/api/BooksBook/Search/20170404",
         {'applicationId': applicationId, 'title': title});
     var response = await http.get(url);
-    return response;
+
     if (response.statusCode == 200) {
-      log.d(response.body);
+      //log.d(response.body);
+      Map<String, dynamic> res = jsonDecode(utf8.decode(response.bodyBytes));
+      for (var json in res["Items"]) {
+        listItems.add(RakutenBooksItem.fromJson(json["Item"]));
+      }
     } else {
       log.d('erro ${{response.statusCode}}');
     }
+    return response;
   }
 }
--- test/rakuten_api_test.dart
+++ test/rakuten_api_test.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart';
 import 'package:logger/logger.dart';
@@ -9,10 +11,24 @@ void main() {
     Logger.level = Level.verbose;
   });
   test('search books by title', () async {
+    List<RakutenBooksItem> listItems = [];
     log.d("search books by title");
     var rakutenApi = RakutenApi();
-    Response response = await rakutenApi.searchBooksByTitle("太陽");
-    log.d(response.body);
+    Response response = await rakutenApi.searchBooksByTitle("太陽", listItems);
+    //log.d(response.body);
     log.d("${response.statusCode}");
+    Map<String, dynamic> res = jsonDecode(utf8.decode(response.bodyBytes));
+    log.d(res);
+    log.d(res['count']);
+    for (var value in res['Items']) {
+      log.d(value['Item']['title']);
+
+      listItems.add(RakutenBooksItem.fromJson(value['Item']));
+    }
+    log.d(listItems.toString());
+
+    RakutenApiResponse apiResponse = RakutenApiResponse.fromJson(res);
+    log.d(apiResponse.genreInformation);
+    log.d(listItems[0].title);
   });
 }
